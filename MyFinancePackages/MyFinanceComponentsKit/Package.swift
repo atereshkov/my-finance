9f61787684d7d228f080fb659477b27ffa2d77d2
Alexander Tereshkov <atserashkou@gmail.com>
2023-02-11 19:55:59 +0100

    Update min supported platform iOS - 15.0 -> 16.0 and swift-tools version

--- MyFinancePackages/MyFinanceComponentsKit/Package.swift
+++ MyFinancePackages/MyFinanceComponentsKit/Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "MyFinanceComponentsKit",
-    platforms: [.iOS(.v15), .macOS(.v11)],
+    platforms: [.iOS(.v16), .macOS(.v11)],
     products: [
         .library(
             name: "MyFinanceComponentsKit",
